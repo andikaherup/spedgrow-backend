d50fc35148ef97ac40e98b2bf7b1938bbae6c4d1
Andika Heru P <andikaherup@gmail.com>
2025-06-15 13:51:54 +0700

    feat: add API routes for transaction management

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TransactionController;
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('transactions', TransactionController::class);
+    Route::get('transactions/nfc/recent', [TransactionController::class, 'recentNfcTransactions']);
+    Route::get('transactions/stats/summary', [TransactionController::class, 'summary']);
+});
