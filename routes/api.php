89ff5f9f77f1c08da2892782d5f6e6ef4aab971a
Andika Heru P <andikaherup@gmail.com>
2025-06-16 14:20:45 +0700

    feat : add middleware cors for api route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransactionController;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware('cors')->group(function () {
     Route::apiResource('transactions', TransactionController::class);
     Route::get('transactions/nfc/recent', [TransactionController::class, 'recentNfcTransactions']);
     Route::get('transactions/stats/summary', [TransactionController::class, 'summary']);
