389140f3a5dbfe6ef1882030c719462e53fba8d9
Andika Heru P <andikaherup@gmail.com>
2025-06-16 14:21:20 +0700

    feat : add option for cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -4,16 +4,21 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-API-Key');
+
+        if ($request->getMethod() === 'OPTIONS') {
+            return response('', 200, $response->headers->all());
+        }
 
         return $response;
     }
